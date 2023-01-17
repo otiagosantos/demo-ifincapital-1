bbfc6b7226e3d89bd5312d073ed1455b3d72af92
Tiago Santos <otiagosantos.code@gmail.com>
2023-01-17 13:45:55 -0300

    Corrige falha de overflow quando item do menu é clicado.

--- js/responsive.js
+++ js/responsive.js
@@ -3,8 +3,8 @@ let menuToggle = document.querySelector('.menu-toggle');
 
 let isMenuToggleActive = false;
 
-function toggleMenu() {
-  isMenuToggleActive = !isMenuToggleActive;
+function toggleMenu(toggle = null) {
+  isMenuToggleActive = toggle !== null ? toggle : !isMenuToggleActive;
   
   navRow.classList.toggle('on', isMenuToggleActive);
   
@@ -16,5 +16,5 @@ menuToggle.addEventListener('click', toggleMenu)
 let menuItens = document.querySelectorAll('.nav-container nav ul li')
 
 menuItens.forEach(item => {
-  item.addEventListener('click', toggleMenu)
+  item.addEventListener('click', () => toggleMenu(false))
 })
